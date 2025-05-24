0261fe20ad58cbba0d7a77c29c6448ba90b977a2
Ashok Kumar <155353341+Ashok8601@users.noreply.github.com>
2025-05-25 00:04:16 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     
     path('', views.home, name='home'),
     path('services/', views.services, name='services'),
-    path('projects/', views.projects, name='projects'),
+
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('login/', views.login_view, name='login'),
