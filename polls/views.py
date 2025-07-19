a9263ced6c5bdf418f616b41154ac62bd5cf4492
Ashok Kumar <155353341+Ashok8601@users.noreply.github.com>
2025-07-19 15:09:40 +0530

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
 
 def services(request):
     return render(request, 'services.html')
