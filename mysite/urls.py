f0ff40a00c2ebeef410cfb2606adde127647bc0d
Ashok Kumar <155353341+Ashok8601@users.noreply.github.com>
2025-07-20 15:19:11 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,24 +4,11 @@ from django.urls import reverse
 from django.urls import path
 from polls import views
 from django.urls import path
-from polls.views import robots_txt
 from django.urls import re_path
 from django.contrib import admin
 
 from django.views.static import serve
 from django.conf import settings
-class StaticViewSitemap(Sitemap):
-    priority = 0.5
-    changefreq = 'weekly'
-
-    def items(self):
-        return ['home', 'indo_pak','services', 'projects', 'about', 'contact', 'login', 'signup','python']
-
-    def location(self, item):
-        return reverse(item)
-
-urlpatterns = [
-    path('sitemap.xml', sitemap, {'sitemaps': {'static': StaticViewSitemap}}, name='django.contrib.sitemaps.views.sitemap'),
     
     path('', views.home, name='home'),
     path('services/', views.services, name='services'),
@@ -30,7 +17,6 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
-    re_path(r'^robots\.txt$', serve, {'path': 'robots.txt', 'document_root': settings.STATIC_ROOT, 'content_type': 'text/plain'}),
     path('indo_pak/', views.indo_pak, name='indo_pak'),
     path('python/', views.python, name='python'),
     path('project/', views.project, name='project'),
